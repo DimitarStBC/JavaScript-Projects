54661c342cbb7e943742e5cdf6126eda2f2407f4
Dimitar Stoev <dimitarg.stoev@gmail.com>
2020-05-05 00:08:06 +0300

    finished project

--- JavaScript-Drum-Kit/javaScript/index.js
+++ JavaScript-Drum-Kit/javaScript/index.js
@@ -1,6 +1,20 @@
-window.addEventListener('keydown', function (e) {
+window.addEventListener('keydown', playing);
+
+function playing(e) {
+
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.box[data-key="${e.keyCode}"]`);
+
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-});
+    key.classList.add('pressed');
+
+}
+function removeClass(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('pressed');
+}
+
+const keys = document.querySelectorAll('.box');
+keys.forEach(key => key.addEventListener('transitionend', removeClass));
